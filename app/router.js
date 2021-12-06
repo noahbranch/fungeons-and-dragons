f104c24ec05b8f6979749416b5c77ccf92cd756b
Noah <noah.branch@healthcaresource.com>
2021-12-06 15:57:55 -0500

    Main page created, couple of components

--- app/router.js
+++ app/router.js
@@ -2,8 +2,13 @@ import EmberRouter from '@ember/routing/router';
 import config from 'fungeons-and-dragons/config/environment';
 
 export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
+	location = config.locationType;
+	rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+	this.route('about');
+	this.route('dungeon-master');
+	this.route('player');
+	this.route('characters');
+});
