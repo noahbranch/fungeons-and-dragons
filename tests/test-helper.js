825234951037f597ab8c6dc7f5d17e26bca646d0
Tomster <tomster@emberjs.com>
2021-12-06 12:53:53 -0500

    Initial Commit from Ember CLI v3.24.0

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'fungeons-and-dragons/app';
+import config from 'fungeons-and-dragons/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
